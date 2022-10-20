726d42b72b6f7f3d807395b1bec848638562add5
Bu huiming <buhm@microport.com>
2022-10-20 12:05:38 +0800

    feature: 设计模块

--- support/src/main/java/com/bhm/sdk/support/IRequest.java
+++ support/src/main/java/com/bhm/sdk/support/IRequest.java
@@ -1,17 +1,22 @@
-package com.bhm.sdk.support
-
-import com.bhm.sdk.support.DownloadConfig
+package com.bhm.sdk.support;
 
 /**
  * @author Buhuiming
  * @date :2022/10/19 15:21
  */
-interface IRequest {
-    fun newRequest(config: DownloadConfig?)
-    fun startDownload(url: String?): Boolean
-    fun startAllDownloads(): Boolean
-    fun pauseDownload(url: String?): Boolean
-    fun pauseAllDownloads(): Boolean
-    fun removeDownload(url: String?): Boolean
-    fun removeAllDownloads(): Boolean
+public interface IRequest {
+
+    void newRequest(DownloadConfig config);
+
+    boolean startDownload(String url);
+
+    boolean startAllDownloads();
+
+    boolean pauseDownload(String url);
+
+    boolean pauseAllDownloads();
+
+    boolean removeDownload(String url);
+
+    boolean removeAllDownloads();
 }
